3cc9c605d48994ba51a2164d19e00948418fb57c
Mehmet Faruk Koç <mfkoc@localhost.localdomain>
2020-06-29 10:58:17 +0300

    Updates

---
+++ precache-manifest.88f88b25483115fdc43e28dd88812ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f14ed9e592ae749c9588b6dc31867b4",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "cbd14d9c4dd0caeda431",
+    "url": "/react-to-do/static/css/main.f7d40f08.chunk.css"
+  },
+  {
+    "revision": "9e62c416c48b0f025e9d",
+    "url": "/react-to-do/static/js/2.731a72a4.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/react-to-do/static/js/2.731a72a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd14d9c4dd0caeda431",
+    "url": "/react-to-do/static/js/main.ade38c5b.chunk.js"
+  },
+  {
+    "revision": "937a9eca39302de3b5e8",
+    "url": "/react-to-do/static/js/runtime-main.7927655d.js"
+  }
+]);
